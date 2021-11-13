f80c492ae0eeaecac8ffc88b8b092db7f91311f5
Raki Rahman <mdrrahman@microsoft.com>
2021-11-13 09:21:52 -0500

    Concurrency

--- parallel-letter-frequency/parallel_letter_frequency.go
+++ parallel-letter-frequency/parallel_letter_frequency.go
@@ -7,8 +7,35 @@ type FreqMap map[rune]int
 // data as a FreqMap.
 func Frequency(s string) FreqMap {
 	m := FreqMap{}
+	// Every time we encounter a rune, we increase frequency in the map by 1
 	for _, r := range s {
 		m[r]++
+	}
+	return m
+}
+
+// Concurrent version of above
+func ConcurrentFrequency(texts []string) FreqMap{
+	// Create a channel to receive the results of the goroutines
+	c := make(chan FreqMap)
+	
+	// For collecting results from Channel
+	m := FreqMap{}
+
+	// Create a function that can be invoked as a goroutine
+	// Returns results to our channel
+	f := func(s string) {
+		c <- Frequency(s)
+	}
+
+	// Invoke goroutine for each text
+	for _ , text := range texts {
+		go f(text)
+
+		// For all the runes per text
+		for k, v := range <- c {
+			m[k] += v
+		}
 	}
 	return m
 }
