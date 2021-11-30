fa49df0d7585034add9f9b7f36ee0db82c1c2a0c
Raki Rahman <mdrrahman@microsoft.com>
2021-11-30 08:24:55 -0500

    Matrix exercise

--- _test/test.go
+++ _test/test.go
@@ -1,197 +1,90 @@
 package main
 
 import (
-	"bytes"
-	"errors"
 	"fmt"
-	"io"
-	"os"
-	"sort"
+	"strconv"
 	"strings"
 )
 
-func cleaner(input string) []string {
-	// Split by newline for each line
-	lines := strings.Split(input, "\n")
-
-	// Make output slice
-	output := make([]string, 0)
-
-	// Loop over and clean each line
-	for _, line := range lines {
-		// Remove tabs from each line
-		line = strings.TrimSpace(line)
-
-		// Remove comments and empty lines
-		if strings.HasPrefix(line, "#") != true && line != "" {
-			output = append(output, line)
-			//fmt.Println("line: ", line)
-		}
-	}
-	return output
+// Define the Matrix type here.
+type Matrix struct {
+	rows     [][]int
+	num_rows int
+	num_cols int
 }
 
-// Struct for storing tally
-type teamScore struct {
-	name string
-	mp   int
-	w    int
-	d    int
-	l    int
-	p    int
+func New(s string) (*Matrix, error) {
+	// Matrix to return
+	var m = Matrix{}
+	// Split by newline to get rows
+	rows := strings.Split(s, "\n")
+	// To store each individual element per row
+	var elements []string
+	// Split each row by space to get individual elements
+	for _, row := range rows {
+		// Trim leading and trailing spaces
+		row = strings.TrimSpace(row)
+		// Split row into elements by splitting by space
+		elements = strings.Split(row, " ")
+		// Clean up each element and convert to integer, append to the row slice
+		new_row := []int{}
+		for _, element := range elements {
+			n, err := strconv.Atoi(strings.TrimSpace(element))
+			// If element cannot be converted to integer, return error
+			if err != nil {
+				return nil, err
+			}
+			// Append element to row slice
+			new_row = append(new_row, n)
+		}
+		// Append row slice to matrix
+		m.rows = append(m.rows, new_row)
+	}
+	// Matrix dimensions
+	m.num_cols = len(m.rows[0])
+	m.num_rows = len(m.rows)
+	// Return pointer to matrix
+	return &m, nil
 }
 
-// Function to generate score from cleaned input
-func generateScore(cleaned []string) []teamScore {
-	var tally = make(map[string]teamScore)
-	var team, opponent, outcome string
-	var teamStruct, opponentStruct teamScore
-
-	for _, line := range cleaned {
-		// Split into data points
-		linesplit := strings.Split(line, ";")
-		team = linesplit[0]
-		opponent = linesplit[1]
-		outcome = linesplit[2]
-
-		// Copy of teamScore for the Team and Opponent
-		teamStruct = tally[team]
-		opponentStruct = tally[opponent]
-
-		// Initialize structs if empty
-		if teamStruct.name == "" {
-			teamStruct = teamScore{name: team}
-		}
-		if opponentStruct.name == "" {
-			opponentStruct = teamScore{name: opponent}
-		}
-
-		// MP: Increment matches played
-		teamStruct.mp++
-		opponentStruct.mp++
-
-		// W: Matches Won
-		if outcome == "win" {
-			teamStruct.w++
-			opponentStruct.l++
-		}
-
-		// D: Matches Drawn (Tied)
-		if outcome == "draw" {
-			teamStruct.d++
-			opponentStruct.d++
-		}
-
-		// L: Matches Lost
-		if outcome == "loss" {
-			teamStruct.l++
-			opponentStruct.w++
-		}
-
-		// P: Points
-		if outcome == "win" {
-			teamStruct.p += 3
-		} else if outcome == "draw" {
-			teamStruct.p += 1
-			opponentStruct.p += 1
-		} else if outcome == "loss" {
-			opponentStruct.p += 3
-		}
-
-		// Update teamStruct to tally
-		tally[team] = teamStruct
-		tally[opponent] = opponentStruct
-	}
-
-	// Convert tally to slice
-	var output = make([]teamScore, 0)
-	for _, team := range tally {
-		output = append(output, team)
-	}
-	return output
+// Cols and Rows must return the results without affecting the matrix.
+func (m *Matrix) Rows() [][]int {
+	return m.rows
 }
 
-// Append whitespace to the end of the string until it is 31 characters long
-func pad(s string) string {
-	for len(s) < 31 {
-		s += " "
-	}
-	return s
-}
-
-// Return score in expected format from tally
-func returnScore(tally []teamScore) string {
-	var output = "Team                           | MP |  W |  D |  L |  P"
-	for _, team := range tally {
-		output += fmt.Sprintf("\n%s| %2d | %2d | %2d | %2d | %2d",
-			pad(team.name), team.mp, team.w, team.d, team.l, team.p)
-	}
-	return output
-}
-
-// Sort tally in expected format
-func sortTally(tally []teamScore) []teamScore {
-	// Sort by points, then by name
-	sort.Slice(tally, func(i, j int) bool {
-		if tally[i].p == tally[j].p {
-			return tally[i].name < tally[j].name
+// Loop over rows and return cols
+func (m *Matrix) Cols() [][]int {
+	var cols [][]int
+	// Loop over num_cols and create columns
+	for i := 0; i < m.num_cols; i++ {
+		new_col := []int{}
+		for j := 0; j < m.num_rows; j++ {
+			// Append element to col slice
+			new_col = append(new_col, m.rows[j][i])
 		}
-		return tally[i].p > tally[j].p
-	})
-	return tally
+		// Append col slice to matrix
+		cols = append(cols, new_col)
+	}
+	return cols
 }
 
-// Function to tally up the score in expected output format
-func Tally(reader io.Reader, writer io.Writer) error {
-	// Split reader into lines
-	buf := new(strings.Builder)
-	_, err := io.Copy(buf, reader)
-
-	// Clean up string
-	cleaned := cleaner(string(buf.String()))
-
-	// Loop over each line
-	for _, line := range cleaned {
-		// Split each line into teams
-		outcome := strings.Split(line, ";")
-
-		// Check for errors
-		if outcome[2] != "win" && outcome[2] != "loss" && outcome[2] != "draw" {
-			return errors.New("Invalid input")
-		}
+func (m *Matrix) Set(row, col, val int) bool {
+	// Out of range
+	if row < 0 || row >= m.num_rows-1 || col < 0 || col >= m.num_cols-1 {
+		return false
+	} else {
+		m.rows[row][col] = val
+		return true
 	}
-
-	// Generate array of teamScores
-	tally := generateScore(cleaned)
-
-	// Sort tally in expected format
-	tally = sortTally(tally)
-
-	// Add returnSCore to buffer
-	_, _ = fmt.Fprintf(writer, returnScore(tally))
-
-	return err
 }
 
 func main() {
-	var input = `
+	var s = "1 2 3\n4 5 6\n7 8 9\n 8 7 6"
+	m, _ := New(s)
+	fmt.Println("rows", m.Rows())
+	fmt.Println("cols", m.Cols())
 
-Allegoric Alaskians;Blithering Badgers;win
-Devastating Donkeys;Allegoric Alaskians;win
-# Catastrophic Loss of the Californians
-Courageous Californians;Blithering Badgers;loss
-
-Blithering Badgers;Devastating Donkeys;loss
-Allegoric Alaskians;Courageous Californians;win
-Devastating Donkeys;Courageous Californians;draw
-    
-    
-    `
-	var buffer bytes.Buffer
-	err := Tally(strings.NewReader(input), &buffer)
-
-	// Print buffer to stdout
-	if err == nil {
-		_, _ = io.Copy(os.Stdout, &buffer)
-	}
+	m.Set(1, 1, 99)
+	fmt.Println("rows", m.Rows())
+	fmt.Println("cols", m.Cols())
 }
