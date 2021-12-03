c781f6fd9c7b19b7d7bdc2ae47a799e72d3a7515
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2021-12-02 21:09:58 -0500

    Matrix solution

--- _test/test.go
+++ _test/test.go
@@ -1,90 +1,14 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-	"strings"
-)
-
-// Define the Matrix type here.
-type Matrix struct {
-	rows     [][]int
-	num_rows int
-	num_cols int
-}
-
-func New(s string) (*Matrix, error) {
-	// Matrix to return
-	var m = Matrix{}
-	// Split by newline to get rows
-	rows := strings.Split(s, "\n")
-	// To store each individual element per row
-	var elements []string
-	// Split each row by space to get individual elements
-	for _, row := range rows {
-		// Trim leading and trailing spaces
-		row = strings.TrimSpace(row)
-		// Split row into elements by splitting by space
-		elements = strings.Split(row, " ")
-		// Clean up each element and convert to integer, append to the row slice
-		new_row := []int{}
-		for _, element := range elements {
-			n, err := strconv.Atoi(strings.TrimSpace(element))
-			// If element cannot be converted to integer, return error
-			if err != nil {
-				return nil, err
-			}
-			// Append element to row slice
-			new_row = append(new_row, n)
-		}
-		// Append row slice to matrix
-		m.rows = append(m.rows, new_row)
-	}
-	// Matrix dimensions
-	m.num_cols = len(m.rows[0])
-	m.num_rows = len(m.rows)
-	// Return pointer to matrix
-	return &m, nil
-}
-
-// Cols and Rows must return the results without affecting the matrix.
-func (m *Matrix) Rows() [][]int {
-	return m.rows
-}
-
-// Loop over rows and return cols
-func (m *Matrix) Cols() [][]int {
-	var cols [][]int
-	// Loop over num_cols and create columns
-	for i := 0; i < m.num_cols; i++ {
-		new_col := []int{}
-		for j := 0; j < m.num_rows; j++ {
-			// Append element to col slice
-			new_col = append(new_col, m.rows[j][i])
-		}
-		// Append col slice to matrix
-		cols = append(cols, new_col)
-	}
-	return cols
-}
-
-func (m *Matrix) Set(row, col, val int) bool {
-	// Out of range
-	if row < 0 || row >= m.num_rows-1 || col < 0 || col >= m.num_cols-1 {
-		return false
-	} else {
-		m.rows[row][col] = val
-		return true
-	}
-}
+import "fmt"
 
 func main() {
-	var s = "1 2 3\n4 5 6\n7 8 9\n 8 7 6"
-	m, _ := New(s)
-	fmt.Println("rows", m.Rows())
-	fmt.Println("cols", m.Cols())
+	s1 := []int{1, 2, 3, 4, 5}
+	s2 := make([]int, len(s1))
 
-	m.Set(1, 1, 99)
-	fmt.Println("rows", m.Rows())
-	fmt.Println("cols", m.Cols())
+	copy(s2, s1)
+	fmt.Println(s1, s2) // [1 2 3 4 5] [1 2 3 4 5]
+
+	s2[1] = 10          // changing s2 does not affect s1
+	fmt.Println(s1, s2) // [1 2 3 4 5] [1 10 3 4 5]
 }
--- matrix/matrix.go
+++ matrix/matrix.go
@@ -1,20 +1,116 @@
 package matrix
 
-// Define the Matrix type here.
+import (
+	"errors"
+	"strconv"
+	"strings"
+)
 
+// Define the Matrix type here.
+type Matrix struct {
+	rows     [][]int
+	num_rows int
+	num_cols int
+}
+
+// Check dimensions of all rows and cols are same
+func SameDimensions (m *Matrix) bool {
+	rows := m.Rows()
+	cols := m.Cols()
+
+	// Loop over rows and check dimensions
+	for i := 0; i < len(rows); i++ {
+		if len(rows[i]) != len(rows[0]) {
+			return false
+		}
+	}
+	// Loop over cols and check dimensions
+	for i := 0; i < len(cols); i++ {
+		if len(cols[i]) != len(cols[0]) {
+			return false
+		}
+	}
+	// All rows and cols have same dimensions
+	return true
+}
+
+// Create a new matrix from a string
 func New(s string) (*Matrix, error) {
-	panic("Please implement the New function")
+	// Matrix to return
+	var m = Matrix{}
+	// Split by newline to get rows slice
+	rows := strings.Split(s, "\n")
+	// To store each individual element per row
+	var elements []string
+	// Loop over rows slice
+	for _, row := range rows {
+		// Trim leading and trailing spaces
+		row = strings.TrimSpace(row)
+		// Split row into elements by splitting by space
+		elements = strings.Split(row, " ")
+		// Clean up each element and convert to integer, append to the row slice
+		new_row := []int{}
+		for _, element := range elements {
+			n, err := strconv.Atoi(strings.TrimSpace(element))
+			// If element cannot be converted to integer, return error
+			if err != nil {
+				return nil, err
+			}
+			// Append element to row slice
+			new_row = append(new_row, n)
+		}
+		// Append row slice to matrix
+		m.rows = append(m.rows, new_row)
+	}
+	// Matrix dimensions
+	m.num_cols = len(m.rows[0])
+	m.num_rows = len(m.rows)
+	// Check dimensions
+	if !SameDimensions(&m) {
+		return nil, errors.New("Matrix dimensions do not match")
+	}
+	// Return pointer to matrix
+	return &m, nil
 }
 
 // Cols and Rows must return the results without affecting the matrix.
-func (m *Matrix) Cols() [][]int {
-	panic("Please implement the Cols function")
+func (m *Matrix) Rows() [][]int {
+	// Because we want to return an independent copy of the slice
+	var rows [][]int
+	var new_row []int
+	// Loop over rows
+	for _, row := range m.rows {
+		new_row = make([]int, len(row))
+		// Copy values
+		copy(new_row, row)
+		// Append to return
+		rows = append(rows, new_row)
+	}
+	return rows
 }
 
-func (m *Matrix) Rows() [][]int {
-	panic("Please implement the Rows function")
+// Loop over rows and return cols
+func (m *Matrix) Cols() [][]int {
+	var cols [][]int
+	// Loop over num_cols and create columns
+	for i := 0; i < m.num_cols; i++ {
+		new_col := []int{}
+		for j := 0; j < m.num_rows; j++ {
+			// Append element to col slice
+			new_col = append(new_col, m.rows[j][i])
+		}
+		// Append col slice to matrix
+		cols = append(cols, new_col)
+	}
+	return cols
 }
 
 func (m *Matrix) Set(row, col, val int) bool {
-	panic("Please implement the Set function")
+	// Out of range
+	if row < 0 || row > m.num_rows-1 || col < 0 || col > m.num_cols-1 {
+		return false
+	} else {
+		m.rows[row][col] = val
+		return true
+	}
 }
