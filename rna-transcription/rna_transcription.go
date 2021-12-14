96646d96c559c239857afd91f1f59d8bc9f2611c
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2021-12-14 02:18:05 -0500

    Done Rna Transcription

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,5 +1,24 @@
 package strand
 
 func ToRNA(dna string) string {
+	rna := ""
 
+	switch len(dna) {
+	case 0:
+		return rna
+	default:
+		for _, value := range dna {
+			switch value {
+			case 'G':
+				rna += string('C')
+			case 'C':
+				rna += string('G')
+			case 'T':
+				rna += string('A')
+			case 'A':
+				rna += string('U')
+			}
+		}
+		return rna
+	}
 }
