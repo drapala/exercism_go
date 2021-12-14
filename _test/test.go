32777054a8a3babde238166bd1cb3e4b6515acfa
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2021-12-14 23:25:40 +0600

    Completed roman numerals

--- _test/test.go
+++ _test/test.go
@@ -1,39 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
-
-// SafeCounter is safe to use concurrently.
-type SafeCounter struct {
-	mu sync.Mutex
-	v  map[string]int
-}
-
-// Inc increments the counter for the given key.
-func (c *SafeCounter) Inc(key string) {
-	c.mu.Lock()
-	// Lock so only one goroutine at a time can access the map c.v.
-	c.v[key]++
-	c.mu.Unlock()
-}
-
-// Value returns the current value of the counter for the given key.
-func (c *SafeCounter) Value(key string) int {
-	c.mu.Lock()
-	// Lock so only one goroutine at a time can access the map c.v.
-	defer c.mu.Unlock()
-	return c.v[key]
-}
+import "fmt"
 
 func main() {
-	c := SafeCounter{v: make(map[string]int)}
-	for i := 0; i < 1000; i++ {
-		go c.Inc("somekey")
-	}
+	digit := 1024
+	ones := digit % 10
+	tens := digit / 10 % 10
+	hundreds := digit / 100 % 10
+	thousands := digit / 1000 % 10
 
-	time.Sleep(time.Second)
-	fmt.Println(c.Value("somekey"))
+	fmt.Println(ones, " ", tens, " ", hundreds, " ", thousands)
 }
--- roman-numerals/roman_numerals.go
+++ roman-numerals/roman_numerals.go
@@ -1,5 +1,116 @@
 package romannumerals
 
+import "fmt"
+
+func convert_thousands(digit int) string {
+	switch digit {
+	case 1:
+		return "M"
+	case 2:
+		return "MM"
+	case 3:
+		return "MMM"
+	case 0:
+		return ""
+	default:
+		return "Invalid digit"
+	}
+}
+
+func convert_hundreds(digit int) string {
+	switch digit {
+	case 1:
+		return "C"
+	case 2:
+		return "CC"
+	case 3:
+		return "CCC"
+	case 4:
+		return "CD"
+	case 5:
+		return "D"
+	case 6:
+		return "DC"
+	case 7:
+		return "DCC"
+	case 8:
+		return "DCCC"
+	case 9:
+		return "CM"
+	case 0:
+		return ""
+	default:
+		return "Invalid digit"
+	}
+}
+
+func convert_tens(digit int) string {
+	switch digit {
+	case 1:
+		return "X"
+	case 2:
+		return "XX"
+	case 3:
+		return "XXX"
+	case 4:
+		return "XL"
+	case 5:
+		return "L"
+	case 6:
+		return "LX"
+	case 7:
+		return "LXX"
+	case 8:
+		return "LXXX"
+	case 9:
+		return "XC"
+	case 0:
+		return ""
+	default:
+		return "Invalid digit"
+	}
+}
+
+func convert_ones(digit int) string {
+	switch digit {
+	case 1:
+		return "I"
+	case 2:
+		return "II"
+	case 3:
+		return "III"
+	case 4:
+		return "IV"
+	case 5:
+		return "V"
+	case 6:
+		return "VI"
+	case 7:
+		return "VII"
+	case 8:
+		return "VIII"
+	case 9:
+		return "IX"
+	case 0:
+		return ""
+	default:
+		return "Invalid digit"
+	}
+}
+
 func ToRomanNumeral(input int) (string, error) {
-	panic("Please implement the ToRomanNumeral function")
+	// Error cases
+	if input < 1 || input > 3000 {
+		return "", fmt.Errorf("number must be between 1 and 3000")
+	}
+
+	// Convert number into it's corresponding digits
+	ones := input % 10
+	tens := input / 10 % 10
+	hundreds := input / 100 % 10
+	thousands := input / 1000 % 10
+
+	final := convert_thousands(thousands) + convert_hundreds(hundreds) + convert_tens(tens) + convert_ones(ones)
+
+	return final, nil
 }
