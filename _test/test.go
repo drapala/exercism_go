2b1b001aced62f05b56247b4eb84965f3a1fd76f
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2021-12-15 15:54:42 +0600

    Strain logic updated

--- _test/test.go
+++ _test/test.go
@@ -2,12 +2,90 @@ package main
 
 import "fmt"
 
-func main() {
-	digit := 1024
-	ones := digit % 10
-	tens := digit / 10 % 10
-	hundreds := digit / 100 % 10
-	thousands := digit / 1000 % 10
+type Ints []int
+type Lists [][]int
+type Strings []string
 
-	fmt.Println(ones, " ", tens, " ", hundreds, " ", thousands)
+// Ints
+func lt10(x int) bool { return x < 10 }
+func gt10(x int) bool { return x > 10 }
+func odd(x int) bool  { return x&1 == 1 }
+func even(x int) bool { return x&1 == 0 }
+
+// Strings
+func zword(s string) bool { return len(s) > 0 && s[0] == 'z' }
+
+// Lists
+func has5(l []int) bool {
+	for _, e := range l {
+		if e == 5 {
+			return true
+		}
+	}
+	return false
+}
+
+func (i Ints) Keep(filter func(int) bool) Ints {
+	var result Ints
+	for _, value := range(i){
+		if filter(value){
+			result = append(result, value)
+		}
+	}
+	return result
+}
+
+func (i Ints) Discard(filter func(int) bool) Ints {
+	var result Ints
+	for _, value := range(i){
+		if !filter(value){
+			result = append(result, value)
+		}
+	}
+	return result
+}
+
+func (s Strings) Keep(filter func(string) bool) Strings {
+	var result Strings
+	for _, value := range(s){
+		if filter(value){
+			result = append(result, value)
+		}
+	}
+	return result
+}
+
+func (l Lists) Keep(filter func([]int) bool) Lists {
+	var result Lists
+	for _, list := range(l){
+		if filter(list){
+			result = append(result, list)
+		}
+	}
+	return result
+}
+
+func main() {
+	// Ints
+	nums := Ints{1, 2, 3, 9, 10, 11}
+	fmt.Println(nums.Keep(lt10))
+	fmt.Println(nums.Discard(lt10))
+	fmt.Println(nums.Keep(odd))
+	fmt.Println(nums.Keep(even))
+
+	// Strings
+	string_list := Strings{"apple", "zebra", "banana", "zombies", "cherimoya", "zealot"}
+	fmt.Println(string_list.Keep(zword))
+
+	// Lists
+	list := Lists{
+		{1, 2, 3},
+		{5, 5, 5},
+		{5, 1, 2},
+		{2, 1, 2},
+		{1, 5, 2},
+		{2, 2, 1},
+		{1, 2, 5},
+	}
+	fmt.Println(list.Keep(has5))
 }
--- strain/strain.go
+++ strain/strain.go
@@ -5,17 +5,41 @@ type Lists [][]int
 type Strings []string
 
 func (i Ints) Keep(filter func(int) bool) Ints {
-	panic("Please implement the Keep function")
+	var result Ints
+	for _, value := range i {
+		if filter(value) {
+			result = append(result, value)
+		}
+	}
+	return result
 }
 
 func (i Ints) Discard(filter func(int) bool) Ints {
-	panic("Please implement the Discard function")
+	var result Ints
+	for _, value := range i {
+		if !filter(value) {
+			result = append(result, value)
+		}
+	}
+	return result
 }
 
 func (l Lists) Keep(filter func([]int) bool) Lists {
-	panic("Please implement the Keep function")
+	var result Lists
+	for _, list := range l {
+		if filter(list) {
+			result = append(result, list)
+		}
+	}
+	return result
 }
 
 func (s Strings) Keep(filter func(string) bool) Strings {
-	panic("Please implement the Keep function")
+	var result Strings
+	for _, value := range s {
+		if filter(value) {
+			result = append(result, value)
+		}
+	}
+	return result
 }
