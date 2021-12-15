fd1270d2a0229f59fc87993ba5733c139fdfd496
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2021-12-15 21:19:53 +0600

    Protein translation

--- etl/etl.go
+++ etl/etl.go
@@ -1,5 +1,15 @@
 package etl
 
+import "strings"
+
 func Transform(in map[int][]string) map[string]int {
-	panic("Please implement the Transform function")
+	var result = make(map[string]int)
+
+	// Loop through the keys and values inside in
+	for key, value := range in {
+		for _, letter := range value {
+			result[strings.ToLower(letter)] = key
+		}
+	}
+	return result
 }
--- protein-translation/protein_translation.go
+++ protein-translation/protein_translation.go
@@ -1,9 +1,59 @@
 package protein
 
-func FromRNA(rna string) ([]string, error) {
-	panic("Please implement the FromRNA function")
-}
+import "fmt"
+
+var ErrStop = fmt.Errorf("stop codon found")
+var ErrInvalidBase = fmt.Errorf("base not recognized")
 
 func FromCodon(codon string) (string, error) {
-	panic("Please implement the FromCodon function")
+	switch codon {
+	case "AUG":
+		return "Methionine", nil
+	case "UUU", "UUC":
+		return "Phenylalanine", nil
+	case "UUA", "UUG":
+		return "Leucine", nil
+	case "UCU", "UCC", "UCA", "UCG":
+		return "Serine", nil
+	case "UAU", "UAC":
+		return "Tyrosine", nil
+	case "UGU", "UGC":
+		return "Cysteine", nil
+	case "UGG":
+		return "Tryptophan", nil
+	case "UAA", "UAG", "UGA":
+		return "", ErrStop
+	default:
+		return "", ErrInvalidBase
+	}
+}
+
+func FromRNA(rna string) ([]string, error) {
+	var result = make([]string, 0)
+
+	// Error handling
+	if len(rna)%3 != 0 {
+		return result, ErrInvalidBase
+	}
+	var protein string
+	var err error
+	// Go through RNA string in 3-character chunks
+	for i := 0; i < len(rna); i += 3{
+		// Encode in chunks of 3
+		protein, err = FromCodon(rna[i:i+3])
+
+		// Error handling
+		if err != nil {
+			if err == ErrStop {
+				// Stop code
+				return result, nil
+			} else if err == ErrInvalidBase {
+				//Invalid codon
+				return nil, err
+			}
+		}
+		// Append protein to result
+		result = append(result, protein)
+	}
+	return result, err
 }
