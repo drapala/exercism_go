fd25e2862becb58c8ef1cddb10b70ffb30a2f8a7
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2021-12-15 21:46:36 +0600

    Completed pangram

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -1,5 +1,29 @@
 package pangram
 
+import (
+	"strings"
+)
+
 func IsPangram(input string) bool {
-	panic("Please implement the IsPangram function")
+
+	// turn input to lowercase
+	input = strings.ToLower(input)
+
+	// To store flags
+	flagger := make(map[rune]bool, 123-97)
+
+	// Loop over string and store flags
+	for _, r := range input {
+		flagger[r] = true
+	}
+
+	var r rune
+	for i := 97; i <= 122; i++ {
+		// Convert i to corresponding rune
+		r = rune(i)
+		if flagger[r] == false {
+			return false
+		}
+	}
+	return true
 }
