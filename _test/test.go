aa92efd45b9af39659fb4bc0ce5d75d4703cb463
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2021-12-16 20:22:53 +0600

    Word count

--- _test/test.go
+++ _test/test.go
@@ -1,24 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"unicode/utf8"
-)
+import "fmt"
 
 func main() {
-	const input = "Hello, 世界"
-
-	store := []rune{}
-	for i, w := 0, 0; i < len(input); i += w {
-		runeValue, width := utf8.DecodeRuneInString(input[i:])
-		w = width
-		store = append(store, runeValue)
-	}
-
-	var result string
-
-	for i := len(store) - 1; i >= 0; i-- {
-		result += string(store[i])
-	}
-	fmt.Println(result)
+	const input = "'large'"
+	fmt.Println(input)
+	fmt.Println(input[1:len(input)-1])
 }
--- word-count/word_count.go
+++ word-count/word_count.go
@@ -1,7 +1,67 @@
 package wordcount
 
+import (
+	"strings"
+)
+
 type Frequency map[string]int
 
+func strip(s string) string {
+    var result strings.Builder
+    for i := 0; i < len(s); i++ {
+        b := s[i]
+        if ('a' <= b && b <= 'z') ||
+            ('A' <= b && b <= 'Z') ||
+            ('0' <= b && b <= '9') ||
+            b == ' ' ||
+			b == '\''{
+            result.WriteByte(b)
+        }
+    }
+    return result.String()
+}
+
+func CleanWord(in string) string {
+	var out string
+
+	// Skip empty strings
+	if in == "" {
+		return ""
+	}
+	// Skip punctuation
+	out = strip(in)
+
+	// Deal with quotations in beginning and end
+	if out[0] == '\'' && out[len(out)-1] == '\'' {
+		out = out[1:len(out)-1]
+	}
+
+	return out
+}
+
 func WordCount(phrase string) Frequency {
-	panic("Please implement the WordCount function")
+	result := make(Frequency)
+
+	// Split by whitespace first
+	// https://pkg.go.dev/strings#Fields
+	whitesplit := strings.Fields(phrase)
+
+	// Split per entry by specific delimiters
+	delimited := make([]string, 0)
+	// https://pkg.go.dev/strings#Split
+	for _, entry := range whitesplit {
+		for _, word := range strings.Split(entry, ",") {
+			// Append to delimited
+			// Filter stuff that didn't get caught:
+			if CleanWord(word) != "" {
+				delimited = append(delimited, CleanWord(word))
+			}
+		}
+	}
+
+	for _, word := range(delimited) {
+		result[strings.ToLower(word)]++
+	}
+
+	return result
 }
