e61212210dc7eedfd58b9fb3eab0f32771633280
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2021-12-16 16:17:07 +0600

    String reversal

--- reverse-string/reverse_string.go
+++ reverse-string/reverse_string.go
@@ -1,5 +1,20 @@
 package reverse
 
+import "unicode/utf8"
+
+// https://go.dev/blog/strings
 func Reverse(input string) string {
-	panic("Please implement the Reverse function")
+	// Store runes in a slice
+	store := []rune{}
+	for i, w := 0, 0; i < len(input); i += w {
+		runeValue, width := utf8.DecodeRuneInString(input[i:])
+		w = width
+		store = append(store, runeValue)
+	}
+	// Reverse and return result
+	var result string
+	for i := len(store) - 1; i >= 0; i-- {
+		result += string(store[i])
+	}
+	return result
 }
