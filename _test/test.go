97c0f677d978d18b0974c59394c18d556150be69
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2021-12-17 11:48:52 +0600

    Completed largest series product

--- _test/test.go
+++ _test/test.go
@@ -5,82 +5,26 @@ import (
 	"strconv"
 )
 
-func findSeqEnd(text string, letterOfInterest string) int{
-	for j := 0; j < len(text); j++ {
-		// If we went over to a new letter, return previous index
-		if string(text[j]) != letterOfInterest {
-			return j-1
-		} else {
-			// Still have a match
-			if j == len(text)-1 {
-				// At the end
-				return j
-			}
-		}
-	}
-	return -1
-}
-
-func RLEResultAppend(input string, count int) string {
-	// Add to result
-	var result string 
-
-	if fmt.Sprint(count) != "1" {
-		result += fmt.Sprint(count)
-	}
-	result += string(input)
-	return result
-}
-
-func findNumEnd(text string) (int, int) {
-	var number int
-	for j := 0; j < len(text); j++ {
-		// If we went over to a letter, number must have ended at previous index
-		if ('0' <= text[j] && text[j] <= '9') {
-			continue
-		} else {
-			// Went over to a letter
-			number, _ = strconv.Atoi(text[:j])
-			return number, j
-		}
-	}
-	return -1, -1
-}
-
-func RLDResultAppend(count int, letter string) string {
-	//fmt.Println("Number: ", count, " | String: ", letter )
-	var result string
-	// Add count number of times
-	for i := 1; i <= count; i++{
-		result += letter
+func CalculateProduct(subdigit string) int {
+	var result int = 1
+	for _, v := range subdigit {
+		conv, _ := strconv.Atoi(string(v))
+		result = result * conv
 	}
 	return result
 }
 
 func main() {
-	input := "12WB12W3B24WB"
-	fmt.Println(input)
+	digits := "1027839564"
+	span := 5
+	perm := len(digits) - span + 1
 
-	var result string
-	var count, local_index int
-
-	for i := 0; i < len(input); i++ {
-		if ('0' <= input[i] && input[i] <= '9') {
-			// Got a number
-			// Find end of number
-			count, local_index = findNumEnd(input[i:])
-			fmt.Println("Number: ", count)
-			// Increment i to start after local_index
-			i += local_index
-
-			// Next must be a single letter
-			fmt.Println("String: ", string(input[i]))
-			result += RLDResultAppend(count, string(input[i]))
-        } else {
-			// Got the string directly
-			fmt.Println("String: ", string(input[i]))
-			result += RLDResultAppend(1, string(input[i]))
+	var max_product int = 0
+	for i := 0; i < perm; i++ {
+		product := CalculateProduct(digits[i:i+span])
+		if max_product < product {
+			max_product = product
 		}
 	}
-	fmt.Println(result)
+	fmt.Println("Max product is: ", max_product)
 }
--- largest-series-product/largest_series_product.go
+++ largest-series-product/largest_series_product.go
@@ -1,5 +1,45 @@
 package lsproduct
 
+import (
+	"fmt"
+	"strconv"
+)
+
+func CheckValidNumber(digit string) bool {
+	for i := 0; i < len(digit); i++ {
+		if digit[i] < '0' || digit[i] > '9' {
+			return false
+		}
+	}
+	return true
+} 
+
+func CalculateProduct(subdigit string) int {
+	var result int = 1
+	for _, v := range subdigit {
+		conv, _ := strconv.Atoi(string(v))
+		result = result * conv
+	}
+	return result
+}
+
 func LargestSeriesProduct(digits string, span int) (int64, error) {
-	panic("Please implement the LargestSeriesProduct function")
+	// Error handling
+	if span > len(digits) {
+		return 0, fmt.Errorf("span must be smaller than string length")
+	} else if span < 0 {
+		return 0, fmt.Errorf("span must not be negative")
+	} else if !CheckValidNumber(digits) {
+		return 0, fmt.Errorf("digits input must only contain digits")
+	}
+	// Number of possible permutations
+	perm := len(digits) - span + 1
+	var max_product int = 0
+	for i := 0; i < perm; i++ {
+		product := CalculateProduct(digits[i:i+span])
+		if max_product < product {
+			max_product = product
+		}
+	}
+	return int64(max_product), nil
 }
