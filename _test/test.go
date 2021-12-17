d96ffda5254a859fd0375506919dd82ce32cd38c
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2021-12-17 08:24:59 +0600

    Updated Test with RLE logic

--- _test/test.go
+++ _test/test.go
@@ -2,8 +2,53 @@ package main
 
 import "fmt"
 
+func findSeqEnd(text string, letterOfInterest string) int{
+	for j := 0; j < len(text); j++ {
+		// If we went over to a new letter, return previous index
+		if string(text[j]) != letterOfInterest {
+			return j-1
+		} else {
+			// Still have a match
+			if j == len(text)-1 {
+				// At the end
+				return j
+			}
+		}
+	}
+	return -1
+}
+
+func ResultAppend(input string, count int) string {
+	// Add to result
+	var result string 
+
+	if fmt.Sprint(count) != "1" {
+		result += fmt.Sprint(count)
+	}
+	result += string(input)
+	return result
+}
+
 func main() {
-	const input = "'large'"
+	input := "AABBBCCCC"
 	fmt.Println(input)
-	fmt.Println(input[1:len(input)-1])
+
+	var result string
+	var local_index, global_index int
+
+	for i := 0; i < len(input); i++ {
+		fmt.Println("Letter of interest: ", string(input[i]), "at i: ", i)
+		local_index = findSeqEnd(input[i:], string(input[i]))
+		global_index = i + local_index
+		fmt.Println("Last ", string(input[i]),  " at global index: ", global_index)
+		fmt.Println("Count: ", local_index + 1)
+		
+		// Add to result
+		result += ResultAppend(string(input[i]), local_index + 1)
+
+		i = global_index
+	}
+
+	fmt.Println(result)
+
 }
--- run-length-encoding/run_length_encoding.go
+++ run-length-encoding/run_length_encoding.go
@@ -1,9 +1,51 @@
 package encode
 
+import "fmt"
+
+func ResultAppend(input string, count int) string {
+	// Add to result
+	var result string 
+
+	if fmt.Sprint(count) != "1" {
+		result += fmt.Sprint(count)
+	}
+	result += string(input)
+	return result
+}
+
 func RunLengthEncode(input string) string {
-	panic("Please implement the RunLengthEncode function")
+	fmt.Println("input: ", input)
+	var result string
+	var slow, fast, count int
+	for slow = 0; slow < len(input); slow++ {
+		fmt.Println("slow: ", slow, " | ", string(input[slow]))
+		count = 1
+		// Loop over and find number of consecutive characters
+		for fast = slow + 1; fast < len(input); fast++ {
+			fmt.Println("fast: ", fast, " | ", string(input[fast]))
+			if input[slow] == input[fast] {
+				fmt.Println("Found ", string(input[fast]), " at ", fast)
+				count++
+				// At the end
+				if fast == len(input){
+					result += ResultAppend(string(input[fast]), count)
+				}
+			} else {
+				// Add to result
+				result += ResultAppend(string(input[fast]), count)
+				
+				// Set slow to next index as it will increment again
+				slow = fast - 1
+				fmt.Println("moving slow: ", slow, " | ", string(input[slow]))
+				break
+			}
+		}
+	}
+	fmt.Println("output:", result)
+	return result
 }
 
 func RunLengthDecode(input string) string {
-	panic("Please implement the RunLengthDecode function")
+	var result string
+	return result
 }
