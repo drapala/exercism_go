0e28479563bb646fec5f1e7ce3b0ef4e9f7f23f5
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2021-12-17 21:34:17 +0600

    Sum of multiples

--- sum-of-multiples/sum_of_multiples.go
+++ sum-of-multiples/sum_of_multiples.go
@@ -1,5 +1,21 @@
 package summultiples
 
 func SumMultiples(limit int, divisors ...int) int {
-	panic("Please implement the SumMultiples function")
+	var multiples []int
+	for n := 1; n < limit; n++ {
+		// Attempt to divide n by each divisor
+		for _, divisor := range divisors {
+			if divisor != 0{
+				if n%divisor == 0 {
+					multiples = append(multiples, n)
+					break
+				}
+			}
+		}
+	}
+	var sum int
+	for _, multiple := range multiples {
+		sum += multiple
+	}
+	return sum
 }
