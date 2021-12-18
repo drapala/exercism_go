a36dda84690e487d7f26fe361896bdaa0a555720
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2021-12-18 09:18:42 +0600

    Crypto Square

--- _test/test.go
+++ _test/test.go
@@ -5,37 +5,90 @@ import (
 	"unicode"
 )
 
-func CalculateCipher(char rune) rune {
-	return rune(int('a') + int(rune('z') - char))
+func normalize(in string) string{
+	var out string
+	for _, c := range(in) {
+		if unicode.IsLetter(c) || unicode.IsNumber(c){
+			out += string(unicode.ToLower(c))
+		}
+	}
+	return out
+}
+
+func findRectangleSpecs(area int) (int, int) {
+	for c := 1; c <= area; c++ {
+		for r := 1; r <= c; r++ {
+			if (r * c >= area) && (c - r <= 1){
+				return r, c
+			}
+		}
+	}
+	// Couldn't find anything for some reason
+	return -1, -1
+}
+
+func padWhitespace(text string, c int) string{
+	var out string = text
+	for i := len(text); i < c; i++ {
+		out += " "
+	}
+	return out
+}
+
+func createRectangle(text string, c int) []string {
+	rectangle := make([]string, 0)
+	var counter int
+	var temp string
+	for i := 0; i < len(text); i++ {
+		temp += string(text[i])
+		counter++
+		// If width is filled up, append block as row and reset for new row
+		if counter == c {
+			rectangle = append(rectangle, temp)
+			temp = ""
+			counter = 0
+		}
+	}
+	// Pad and append left over
+	if temp != "" {
+		rectangle = append(rectangle, padWhitespace(temp, c))
+	}
+	return rectangle
+}
+
+func createCipherFromRectangle(rectangle []string) string{
+	var cipher string
+	r := len(rectangle)
+	c := len(rectangle[0])
+	for col := 0; col < c; col++ {
+		for row := 0; row < r; row++ {
+			cipher += string(rectangle[row][col])
+			if row == r - 1{
+				cipher += " "
+			}
+		}
+	}
+	return cipher
 }
 
 func main() {
-	var s string = "Testing,1 2 3, testing."
+	var s string = "If man was meant to stay on the ground, god would have given us roots."
 	fmt.Println("Input: ", s)
 
-	var output string
-	var count int
-	var lower rune
+	var normalized string = normalize(s)
+	fmt.Println("Normalized: ", normalized)
+	fmt.Println("Length: ", len(normalized))
 
-	for _, char := range(s) {
-		lower = unicode.ToLower(char)
-		// Only proceed for alphabets and numerics
-		if (lower >= 'a' && lower <= 'z') || (lower >= '0' && lower <= '9') {
-			// Group by 5
-			if count == 5 {
-				output += " "
-				count = 0
-			}
-			if (lower >= 'a' && lower <= 'z') {
-				// Deal with alphabets
-				output += string(CalculateCipher(lower))
-			} else if (lower >= '0' && lower <= '9') {
-				// Deal with numerics
-				output += string(char)
-			}
-			// Increment count
-			count++
-		}
-	}
-	fmt.Println("Output: ", output)
+	var r, c int
+	r, c =  findRectangleSpecs(len(normalized))
+	fmt.Println("r:", r, "c:", c)
+
+	var rectangle []string
+	rectangle = createRectangle(normalized, c)
+	fmt.Println(rectangle)
+
+	var cipher string
+	cipher = createCipherFromRectangle(rectangle)
+	fmt.Println(cipher)
+
 }
--- crypto-square/crypto_square.go
+++ crypto-square/crypto_square.go
@@ -1,5 +1,91 @@
 package cryptosquare
 
+import (
+	"unicode"
+)
+
+func normalize(in string) string {
+	var out string
+	for _, c := range in {
+		if unicode.IsLetter(c) || unicode.IsNumber(c) {
+			out += string(unicode.ToLower(c))
+		}
+	}
+	return out
+}
+
+func findRectangleSpecs(area int) (int, int) {
+	for c := 1; c <= area; c++ {
+		for r := 1; r <= c; r++ {
+			if (r*c >= area) && (c-r <= 1) {
+				return r, c
+			}
+		}
+	}
+	// Couldn't find anything for some reason
+	return -1, -1
+}
+
+func padWhitespace(text string, c int) string {
+	var out string = text
+	for i := len(text); i < c; i++ {
+		out += " "
+	}
+	return out
+}
+
+func createRectangle(text string, c int) []string {
+	rectangle := make([]string, 0)
+	var counter int
+	var temp string
+	for i := 0; i < len(text); i++ {
+		temp += string(text[i])
+		counter++
+		// If width is filled up, append block as row and reset for new row
+		if counter == c {
+			rectangle = append(rectangle, temp)
+			temp = ""
+			counter = 0
+		}
+	}
+	// Pad and append left over
+	if temp != "" {
+		rectangle = append(rectangle, padWhitespace(temp, c))
+	}
+	return rectangle
+}
+
+func createCipherFromRectangle(rectangle []string) string {
+	var cipher string
+	r := len(rectangle)
+	c := len(rectangle[0])
+	for col := 0; col < c; col++ {
+		for row := 0; row < r; row++ {
+			cipher += string(rectangle[row][col])
+			if (row == r-1) && (col != c-1) {
+				// If we're not at the last entry, append whitespace
+				cipher += " "
+			}
+		}
+	}
+	return cipher
+}
+
 func Encode(pt string) string {
-	panic("Please implement the Encode function")
+	// Edge case
+	if len(pt) == 0 {
+		return pt
+	}
+	// Normalize string
+	var normalized string = normalize(pt)
+	// Determine rectangle specs
+	var c int
+	_, c =  findRectangleSpecs(len(normalized))
+	// Fill rectangle
+	var rectangle []string
+	rectangle = createRectangle(normalized, c)
+	// Generate cipher from rectange
+	var cipher string
+	cipher = createCipherFromRectangle(rectangle)
+	return cipher
 }
