e9da79be8dd2c8db13b7bee031bad78cbb9e31b1
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2021-12-21 17:06:25 +0600

    Completed darts

--- darts/darts.go
+++ darts/darts.go
@@ -1,5 +1,23 @@
 package darts
 
+import "math"
+
 func Score(x, y float64) int {
-	panic("Please implement the Score function")
+	// Find radius of point
+	var r float64 = math.Sqrt(x*x + y*y)
+
+	switch {
+	case r <= 1.0:
+		// Inner circle
+		return 10
+	case r <= 5.0 && r > 1.0:
+		// Middle circle
+		return 5
+	case r <= 10.0 && r > 5.0:
+		// Outer circle
+		return 1
+	default:
+		// Missed target
+		return 0
+	}
 }
