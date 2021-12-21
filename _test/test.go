d9ad287ced0f69fae2291f2fb4906878c53ea0b0
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2021-12-21 11:45:39 +0600

    Implemented foldLeft

--- _test/test.go
+++ _test/test.go
@@ -2,56 +2,7 @@ package main
 
 import "fmt"
 
-type nums struct {
-	value int
-	numtype string
-}
-
-func generateNums(limit int) []nums {
-	numArrays := make([]nums, 0)
-	for i := 2; i <= limit ; i++ {
-		currentNum := nums{value: i, numtype: "tbd"}
-		numArrays = append(numArrays, currentNum)
-	}
-	return numArrays
-}
-
-func filterprimes(numArray []nums) []int {
-	primes := make([]int, 0)
-	for i:=0; i < len(numArray); i++ {
-		if numArray[i].numtype == "prime" {
-			primes = append(primes, numArray[i].value)
-		}
-	}
-	return primes
-}
-
 func main() {
-	var limit int = 13
-
-	var numArray []nums
-	numArray = generateNums(limit)
-
-	for i := 0; i < len(numArray); i++ {
-		currentVal := numArray[i].value
-		if numArray[i].numtype == "composite"{
-			continue
-		} else if numArray[i].numtype == "tbd" {
-			// Set current to prime
-			numArray[i].numtype = "prime"
-			// index = currentVal - 2
-			// So we add currentVal to get the next multiple's index
-			nextIndex := (currentVal - 2) + currentVal
-			// While loop to mark the rest of the multiples as composites
-			for nextIndex < len(numArray) {
-				numArray[nextIndex].numtype = "composite"
-				nextIndex += currentVal
-			}
-		}			
-	}
-	fmt.Println(numArray)
-
-	primes := filterprimes(numArray)
-	fmt.Println(primes)
-
+	fmt.Println((5 / 2))
+	fmt.Println((5 / 2) / 5)
 }
--- list-ops/list_ops.go
+++ list-ops/list_ops.go
@@ -1,14 +1,37 @@
 package listops
 
+import "fmt"
+
 // IntList is an abstraction of a list of integers which we can define methods on
 type IntList []int
 
+// Fold: 
+// https://en.wikipedia.org/wiki/Fold_%28higher-order_function%29
+// https://burgaud.com/foldl-foldr-python
+
 func (s IntList) Foldl(fn func(int, int) int, initial int) int {
-	panic("Please implement the Foldl function")
+	if len(s) == 0 {
+		return initial
+	}
+
+	fmt.Println(s)
+	// 1: [1 2 3 4]
+	// 2: [2 3 4]
+	// 3: [3 4]
+	// 4: [4]
+	// 5: []
+
+	val := s[1:].Foldl(fn, fn(initial, s[0]))
+	fmt.Println("Returned:", val)
+	return val
+	// 1: [2 3 4].Foldl(fn, fn(1, 5))
+	// 2: [3 4].Foldl(fn, fn(2, 6))
+	// 3: [4].Foldl(fn, fn(3, 8))
+	// 4: [].Foldl(fn, fn(4, 11))
 }
 
 func (s IntList) Foldr(fn func(int, int) int, initial int) int {
-	panic("Please implement the Foldl function")
+	panic("Please implement the Foldr function")
 }
 
 func (s IntList) Filter(fn func(int) bool) IntList {
