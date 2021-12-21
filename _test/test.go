0b05141004f472b6a86e549a3ebadc10ae82f136
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2021-12-21 18:28:29 +0600

    Completed Bob

--- _test/test.go
+++ _test/test.go
@@ -1,11 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+func preProcess(s string) string {
+	result := s 
+	// Remove leading and trailing spaces
+	result = strings.TrimSpace(s)
+	return result
+}
+
+func IsAllUpper(s string) bool {
+    for _, r := range s {
+        if !unicode.IsUpper(r) && unicode.IsLetter(r) {
+            return false
+        }
+    }
+    return true
+}
+
+func IsQuestion(s string) bool {
+	if s[len(s)-1] == '?' {
+		return true
+	}
+    return false
+}
 
 func main() {
-	var myList = make([]int, 2)
-	myList[0] = 1
-	myList[1] = 2
-	fmt.Println(myList)
-	myList[2] = 3
+	s1 := "   Tom-ay-to, tom-aaaah-to.  "
+	s1 = preProcess(s1)
+	fmt.Println("s1 is Uppercase?", IsAllUpper(s1))
+
+	s2 := "  WHAT'S GOING ON?   "
+	s2 = preProcess(s2)
+	fmt.Println("s2 is question?", IsQuestion(s2))
+	fmt.Println("s2 is Uppercase?", IsAllUpper(s2))
 }
--- bob/bob.go
+++ bob/bob.go
@@ -1,15 +1,66 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package bob should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package bob
 
-// Hey should have a comment documenting it.
+import (
+	"strings"
+	"unicode"
+)
+
+func preProcess(s string) string {
+	result := s
+	// Remove leading and trailing spaces
+	result = strings.TrimSpace(s)
+	return result
+}
+
+func IsAllUpper(s string) bool {
+	var isAllNumerics bool = true
+	for _, r := range s {
+		// If a single letter is not upper, is not all upper
+		if !unicode.IsUpper(r) && unicode.IsLetter(r) {
+			return false
+		}
+		// If we find a non-numeric character, is not all numerics
+		if unicode.IsLetter(r) {
+			isAllNumerics = false
+		}
+	}
+	// If all numerics, return false since it's not uppercase
+	if isAllNumerics {
+		return false
+	} else {
+		return true
+	}
+}
+
+func IsQuestion(s string) bool {
+	if s[len(s)-1] == '?' {
+		return true
+	}
+	return false
+}
+
 func Hey(remark string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	// Pre-process the remark
+	remark = preProcess(remark)
+
+	// Check if silence
+	if len(remark) == 0 {
+		return "Fine. Be that way!"
+	}
+	// Check if question
+	if IsQuestion(remark) {
+		// Check if uppercase
+		if IsAllUpper(remark) {
+			return "Calm down, I know what I'm doing!"
+		} else {
+			return "Sure."
+		}
+	} else {
+		// Check if uppercase
+		if IsAllUpper(remark) {
+			return "Whoa, chill out!"
+		} else {
+			return "Whatever."
+		}
+	}
 }
