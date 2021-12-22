72d62c57c481cf69cc93b34d97c5fda1c1d06fa2
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2021-12-22 11:37:07 +0600

    Completed Collatz Conjecture

--- _test/test.go
+++ _test/test.go
@@ -2,40 +2,9 @@ package main
 
 import (
 	"fmt"
-	"strings"
-	"unicode"
 )
 
-func preProcess(s string) string {
-	result := s 
-	// Remove leading and trailing spaces
-	result = strings.TrimSpace(s)
-	return result
-}
-
-func IsAllUpper(s string) bool {
-    for _, r := range s {
-        if !unicode.IsUpper(r) && unicode.IsLetter(r) {
-            return false
-        }
-    }
-    return true
-}
-
-func IsQuestion(s string) bool {
-	if s[len(s)-1] == '?' {
-		return true
-	}
-    return false
-}
-
 func main() {
-	s1 := "   Tom-ay-to, tom-aaaah-to.  "
-	s1 = preProcess(s1)
-	fmt.Println("s1 is Uppercase?", IsAllUpper(s1))
-
-	s2 := "  WHAT'S GOING ON?   "
-	s2 = preProcess(s2)
-	fmt.Println("s2 is question?", IsQuestion(s2))
-	fmt.Println("s2 is Uppercase?", IsAllUpper(s2))
+	var n int = 1
+	fmt.Println(n%2)
 }
--- collatz-conjecture/collatz_conjecture.go
+++ collatz-conjecture/collatz_conjecture.go
@@ -1,5 +1,22 @@
 package collatzconjecture
 
+import "fmt"
+
 func CollatzConjecture(n int) (int, error) {
-	panic("Please implement the CollatzConjecture function")
+	var count int
+
+	if n <= 0 {
+		return 0, fmt.Errorf("%d is not a positive number", n)
+	}
+
+	// Loop until we get to 1
+	for n != 1 {
+		count++
+		if n%2 == 0 { // Even
+			n = n / 2
+		} else { // Odd
+			n = 3*n + 1
+		}
+	}
+	return count, nil
 }
