43b356461e414a65980db89c0b2319b08d76abcd
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2021-12-22 19:37:16 +0600

    Armstrong numbers

--- armstrong-numbers/armstrong_numbers.go
+++ armstrong-numbers/armstrong_numbers.go
@@ -1,5 +1,25 @@
 package armstrong
 
+import (
+	"math"
+	"strconv"
+)
+
+func numdigits(n int) int {
+	s := strconv.Itoa(n)
+	return len(s)
+}
+
+func calcArmstrongValue(n, length int) int {
+	s := strconv.Itoa(n)
+	var result float64
+	for _, c := range s {
+		v, _ := strconv.Atoi(string(c))
+		result += math.Pow(float64(v), float64(length))
+	}
+	return int(result)
+}
+
 func IsNumber(n int) bool {
-	panic("Please implement the IsNumber function")
+	return calcArmstrongValue(n, numdigits(n)) == n
 }
