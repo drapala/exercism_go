d5807a2c59f3dbfa6783cd6ba7a967d5b171fff6
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2021-12-22 20:24:29 +0600

    Completed Flatten Array

--- flatten-array/flatten_array.go
+++ flatten-array/flatten_array.go
@@ -1,5 +1,18 @@
 package flatten
 
+func getNestedTypes(nested interface{}, result *[]interface{}) {
+	switch nested.(type) {
+	case int:
+		*result = append(*result, nested)
+	case []interface{}:
+		for _, v := range nested.([]interface{}) { // Use type assertion to loop over []interface{}
+			getNestedTypes(v, result) // Recursively call getNestedTypes
+		}
+	}
+}
+
 func Flatten(nested interface{}) []interface{} {
-	panic("Please implement the Flatten function")
+	result := make([]interface{}, 0)
+	getNestedTypes(nested, &result)
+	return result
 }
