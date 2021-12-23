6ed4bba303c998e3fdb330f6725f86bb31a3b49f
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2021-12-23 16:09:02 +0600

    Completed Prime Factors

--- prime-factors/prime_factors.go
+++ prime-factors/prime_factors.go
@@ -1,5 +1,21 @@
 package prime
 
+func getDivisors(n int64, divisors *[]int64, testDivisor int64) {
+	// If we're at 1, we're done
+	if n == 1 {
+		return
+	}
+	if n%testDivisor == 0 { // Divisor is good, try again
+		*divisors = append(*divisors, testDivisor)
+		getDivisors(n/testDivisor, divisors, testDivisor)
+	} else { // Divisor is not good, increment
+		// Recurse
+		getDivisors(n, divisors, testDivisor+1)
+	}
+}
+
 func Factors(n int64) []int64 {
-	panic("Please implement the Factors function")
+	divisors := make([]int64, 0)
+	getDivisors(n, &divisors, 2) // Start with 2
+	return divisors
 }
