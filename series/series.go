53479d30ae4fba618a37ecfd22b09dde45678537
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2021-12-23 16:49:30 +0600

    Completed Series.go

--- series/series.go
+++ series/series.go
@@ -1,9 +1,29 @@
 package series
 
 func All(n int, s string) []string {
-	panic("Please implement the All function")
+	result := make([]string, 0)
+	// Error handling
+	if n > len(s) {
+		return result // Empty slice
+	}
+	start := 0
+	end := n
+	for end <= len(s) {
+		result = append(result, s[start:end])
+		start++
+		end++
+	}
+	return result
 }
 
 func UnsafeFirst(n int, s string) string {
-	panic("Please implement the UnsafeFirst function")
+	return All(n, s)[0]
+}
+
+func First(n int, s string) (first string, ok bool) {
+	if n > len(s) {
+		return "", false
+	} else {
+		return All(n, s)[0], true
+	}
 }
