c351a9a8cbf7a49365a8868eb6650a12ebf38e63
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2021-12-23 19:57:25 +0600

    Completed matching brackets

--- _test/test.go
+++ _test/test.go
@@ -1,10 +1,27 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+func contains(s []rune, e rune) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
 
 func main() {
-	var n int = 1
-	fmt.Println(n%2)
+	// input := "\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)"
+	input := "(((185 + 223.85) * 15) - 543)/2"
+
+	var cleaned string
+	match := []rune{'[', ']', '(', ')', '{', '}'}
+
+	for _, c := range input {
+		if contains(match, c) {
+			cleaned += string(c)
+		}
+	}
+	fmt.Println(cleaned)
 }
--- matching-brackets/matching_brackets.go
+++ matching-brackets/matching_brackets.go
@@ -1,5 +1,60 @@
 package brackets
 
+func contains(s []rune, e rune) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+func BracketSequenceCorrect(input string) bool {
+	var stack []string
+	open := []rune{'[', '(', '{'}
+	close := []rune{']', ')', '}'}
+
+	for _, c := range input {
+		if contains(open, c) { // Open Bracket
+			stack = append(stack, string(c)) // Push to stack
+		} else if contains(close, c) { // Close Bracket
+			if len(stack) == 0 { // Hit closing Bracket with empty stack
+				return false
+			}
+			peek := stack[len(stack)-1] // Peek 
+			switch c{
+			case ']':
+				if peek != "[" {
+					return false
+				} else {
+					stack = stack[:len(stack)-1] // Pop
+				}
+			case ')':
+				if peek != "(" {
+					return false
+				} else {
+					stack = stack[:len(stack)-1] // Pop
+				}
+			case '}':
+				if peek != "{" {
+					return false
+				} else {
+					stack = stack[:len(stack)-1] // Pop
+				}
+			}
+		}
+	}
+
+	return len(stack) == 0
+}
+
 func Bracket(input string) bool {
-	panic("Please implement the Bracket function")
+	var cleaned string
+	match := []rune{'[', ']', '(', ')', '{', '}'}
+	for _, c := range input {
+		if contains(match, c) {
+			cleaned += string(c)
+		}
+	}
+	return BracketSequenceCorrect(cleaned)
 }
