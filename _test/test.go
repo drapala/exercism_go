2624bf4b32bf8142f8d03aec1bd88c27ba4b56ef
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2021-12-26 20:00:50 +0600

    Completed Food Chain

--- _test/test.go
+++ _test/test.go
@@ -1,45 +1,118 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
-func Verses(start, stop int) (string, error) {
+type verse struct {
+	current_animal   string
+	comment          string
+	previous_animals []string
+}
+
+var verse_1 = verse{
+	current_animal:   "fly",
+	comment:          "",
+	previous_animals: []string{},
+}
+var verse_2 = verse{
+	current_animal:   "spider",
+	comment:          "It wriggled and jiggled and tickled inside her.",
+	previous_animals: []string{"spider","fly"},
+}
+var verse_3 = verse{
+	current_animal:   "bird",
+	comment:          "How absurd to swallow a bird!",
+	previous_animals: []string{"bird","spider", "fly"},
+}
+var verse_4 = verse{
+	current_animal:   "cat",
+	comment:          "Imagine that, to swallow a cat!",
+	previous_animals: []string{"cat", "bird", "spider", "fly"},
+}
+var verse_5 = verse{
+	current_animal:   "dog",
+	comment:          "What a hog, to swallow a dog!",
+	previous_animals: []string{"dog", "cat", "bird", "spider", "fly"},
+}
+var verse_6 = verse{
+	current_animal:   "goat",
+	comment:          "Just opened her throat and swallowed a goat!",
+	previous_animals: []string{"goat", "dog", "cat", "bird", "spider", "fly"},
+}
+var verse_7 = verse{
+	current_animal:   "cow",
+	comment:          "I don't know how she swallowed a cow!",
+	previous_animals: []string{"cow", "goat", "dog", "cat", "bird", "spider", "fly"},
+}
+var verse_8 = verse{
+	current_animal:   "horse",
+	comment:          "She's dead, of course!",
+	previous_animals: []string{},
+}
+
+var verse_map map[int]verse = map[int]verse{
+	1: verse_1,
+	2: verse_2,
+	3: verse_3,
+	4: verse_4,
+	5: verse_5,
+	6: verse_6,
+	7: verse_7,
+	8: verse_8,
+}
+
+func NarratePreviousAnimals(verse_n verse) string {
+	if len(verse_n.previous_animals) == 0 { // no looping logic
+		return ""
+	}
 	var result string
 
-	for n := start; n >= stop; n-- {
-		verse, err := Verse(n)
-		if err != nil {
-			return "", err
+	for i := 1; i < len(verse_n.previous_animals); i++ {
+		if verse_n.previous_animals[i] == "spider" {
+			result += fmt.Sprintf("\nShe swallowed the %s to catch the %s that wriggled and jiggled and tickled inside her.", verse_n.previous_animals[i-1], verse_n.previous_animals[i])
+		} else {
+			result += fmt.Sprintf("\nShe swallowed the %s to catch the %s.", verse_n.previous_animals[i-1], verse_n.previous_animals[i])
 		}
-		result += verse + "\n"
 	}
-
-	return result, nil
+	return result + "\n"
 }
 
-func nBottles(n int) string {
-	return fmt.Sprintf("%d bottles of beer on the wall, %d bottles of beer.\n", n, n)
-}
+func Verse(v int) string {
+	var result string
 
-func Verse(n int) (string, error) {
-	if n == 0 {
-		return "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n", nil
-	} else if n > 1 && n <= 99 {
-		return (nBottles(n) + fmt.Sprintf("Take one down and pass it around, %d bottles of beer on the wall.\n",n-1)), nil
-	} else if n == 1 {
-		return fmt.Sprintf("%d bottle of beer on the wall, %d bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n", n, n), nil
+	verse_n := verse_map[v]
+
+	// Add first line and comment to all
+	result += fmt.Sprintf("I know an old lady who swallowed a %s.", verse_n.current_animal)
+	result += fmt.Sprintf("\n%s", verse_n.comment)
+
+	// Add previous animal narrations
+	result += NarratePreviousAnimals(verse_n)
+
+	// Add static comment if not last
+	if v!=8 {
+		result += "I don't know why she swallowed the fly. Perhaps she'll die."
 	}
-	return "", errors.New("n out of bounds")
+
+	return result
 }
 
 func main() {
-	fmt.Println(Verse(8))
-	fmt.Println(Verse(3))
-	fmt.Println(Verse(2))
 	fmt.Println(Verse(1))
-	fmt.Println(Verse(0))
-
-	fmt.Println(Verses(8, 6))
+	fmt.Println("\n")
+	fmt.Println(Verse(2))
+	fmt.Println("\n")
+	fmt.Println(Verse(3))
+	fmt.Println("\n")
+	fmt.Println(Verse(4))
+	fmt.Println("\n")
+	fmt.Println(Verse(5))
+	fmt.Println("\n")
+	fmt.Println(Verse(6))
+	fmt.Println("\n")
+	fmt.Println(Verse(7))
+	fmt.Println("\n")
+	fmt.Println(Verse(8))
+	fmt.Println("\n")
 }
--- food-chain/food_chain.go
+++ food-chain/food_chain.go
@@ -1,13 +1,113 @@
 package foodchain
 
+import (
+	"fmt"
+	"strings"
+)
+
+type verse struct {
+	current_animal   string
+	comment          string
+	previous_animals []string
+}
+
+var verse_1 = verse{
+	current_animal:   "fly",
+	comment:          "",
+	previous_animals: []string{},
+}
+var verse_2 = verse{
+	current_animal:   "spider",
+	comment:          "It wriggled and jiggled and tickled inside her.",
+	previous_animals: []string{"spider","fly"},
+}
+var verse_3 = verse{
+	current_animal:   "bird",
+	comment:          "How absurd to swallow a bird!",
+	previous_animals: []string{"bird","spider", "fly"},
+}
+var verse_4 = verse{
+	current_animal:   "cat",
+	comment:          "Imagine that, to swallow a cat!",
+	previous_animals: []string{"cat", "bird", "spider", "fly"},
+}
+var verse_5 = verse{
+	current_animal:   "dog",
+	comment:          "What a hog, to swallow a dog!",
+	previous_animals: []string{"dog", "cat", "bird", "spider", "fly"},
+}
+var verse_6 = verse{
+	current_animal:   "goat",
+	comment:          "Just opened her throat and swallowed a goat!",
+	previous_animals: []string{"goat", "dog", "cat", "bird", "spider", "fly"},
+}
+var verse_7 = verse{
+	current_animal:   "cow",
+	comment:          "I don't know how she swallowed a cow!",
+	previous_animals: []string{"cow", "goat", "dog", "cat", "bird", "spider", "fly"},
+}
+var verse_8 = verse{
+	current_animal:   "horse",
+	comment:          "She's dead, of course!",
+	previous_animals: []string{},
+}
+
+var verse_map map[int]verse = map[int]verse{
+	1: verse_1,
+	2: verse_2,
+	3: verse_3,
+	4: verse_4,
+	5: verse_5,
+	6: verse_6,
+	7: verse_7,
+	8: verse_8,
+}
+
+func NarratePreviousAnimals(verse_n verse) string {
+	if len(verse_n.previous_animals) == 0 { // no looping logic
+		return ""
+	}
+	var result string
+
+	for i := 1; i < len(verse_n.previous_animals); i++ {
+		if verse_n.previous_animals[i] == "spider" {
+			result += fmt.Sprintf("\nShe swallowed the %s to catch the %s that wriggled and jiggled and tickled inside her.", verse_n.previous_animals[i-1], verse_n.previous_animals[i])
+		} else {
+			result += fmt.Sprintf("\nShe swallowed the %s to catch the %s.", verse_n.previous_animals[i-1], verse_n.previous_animals[i])
+		}
+	}
+	return result + "\n"
+}
+
 func Verse(v int) string {
-	panic("Please implement the Verse function")
+	var result string
+
+	verse_n := verse_map[v]
+
+	// Add first line and comment to all
+	result += fmt.Sprintf("I know an old lady who swallowed a %s.", verse_n.current_animal)
+	result += fmt.Sprintf("\n%s", verse_n.comment)
+
+	// Add previous animal narrations
+	result += NarratePreviousAnimals(verse_n)
+
+	// Add static comment if not last
+	if v!=8 {
+		result += "I don't know why she swallowed the fly. Perhaps she'll die."
+	}
+
+	return result
 }
 
 func Verses(start, end int) string {
-	panic("Please implement the Verses function")
+	var result string
+	for v := start; v <= end; v++ {
+		verse := Verse(v)
+		result += verse + "\n\n"
+	}
+	return strings.TrimSpace(result)
 }
 
 func Song() string {
-	panic("Please implement the Song function")
+	return Verses(1, 8)
 }
