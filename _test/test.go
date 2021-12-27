d31d0cac9a2531b132525da68ae692820e8abba9
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2021-12-27 16:42:05 +0600

    Completed Diffie Hellman

--- _test/test.go
+++ _test/test.go
@@ -2,117 +2,14 @@ package main
 
 import (
 	"fmt"
+	"math/big"
 )
 
-type verse struct {
-	current_animal   string
-	comment          string
-	previous_animals []string
-}
-
-var verse_1 = verse{
-	current_animal:   "fly",
-	comment:          "",
-	previous_animals: []string{},
-}
-var verse_2 = verse{
-	current_animal:   "spider",
-	comment:          "It wriggled and jiggled and tickled inside her.",
-	previous_animals: []string{"spider","fly"},
-}
-var verse_3 = verse{
-	current_animal:   "bird",
-	comment:          "How absurd to swallow a bird!",
-	previous_animals: []string{"bird","spider", "fly"},
-}
-var verse_4 = verse{
-	current_animal:   "cat",
-	comment:          "Imagine that, to swallow a cat!",
-	previous_animals: []string{"cat", "bird", "spider", "fly"},
-}
-var verse_5 = verse{
-	current_animal:   "dog",
-	comment:          "What a hog, to swallow a dog!",
-	previous_animals: []string{"dog", "cat", "bird", "spider", "fly"},
-}
-var verse_6 = verse{
-	current_animal:   "goat",
-	comment:          "Just opened her throat and swallowed a goat!",
-	previous_animals: []string{"goat", "dog", "cat", "bird", "spider", "fly"},
-}
-var verse_7 = verse{
-	current_animal:   "cow",
-	comment:          "I don't know how she swallowed a cow!",
-	previous_animals: []string{"cow", "goat", "dog", "cat", "bird", "spider", "fly"},
-}
-var verse_8 = verse{
-	current_animal:   "horse",
-	comment:          "She's dead, of course!",
-	previous_animals: []string{},
-}
-
-var verse_map map[int]verse = map[int]verse{
-	1: verse_1,
-	2: verse_2,
-	3: verse_3,
-	4: verse_4,
-	5: verse_5,
-	6: verse_6,
-	7: verse_7,
-	8: verse_8,
-}
-
-func NarratePreviousAnimals(verse_n verse) string {
-	if len(verse_n.previous_animals) == 0 { // no looping logic
-		return ""
-	}
-	var result string
-
-	for i := 1; i < len(verse_n.previous_animals); i++ {
-		if verse_n.previous_animals[i] == "spider" {
-			result += fmt.Sprintf("\nShe swallowed the %s to catch the %s that wriggled and jiggled and tickled inside her.", verse_n.previous_animals[i-1], verse_n.previous_animals[i])
-		} else {
-			result += fmt.Sprintf("\nShe swallowed the %s to catch the %s.", verse_n.previous_animals[i-1], verse_n.previous_animals[i])
-		}
-	}
-	return result + "\n"
-}
-
-func Verse(v int) string {
-	var result string
-
-	verse_n := verse_map[v]
-
-	// Add first line and comment to all
-	result += fmt.Sprintf("I know an old lady who swallowed a %s.", verse_n.current_animal)
-	result += fmt.Sprintf("\n%s", verse_n.comment)
-
-	// Add previous animal narrations
-	result += NarratePreviousAnimals(verse_n)
-
-	// Add static comment if not last
-	if v!=8 {
-		result += "I don't know why she swallowed the fly. Perhaps she'll die."
-	}
-
-	return result
+// Calculates g^a
+func PowerBig(g, a *big.Int) *big.Int {
+	return new(big.Int).Exp(g, a, nil)
 }
 
 func main() {
-	fmt.Println(Verse(1))
-	fmt.Println("\n")
-	fmt.Println(Verse(2))
-	fmt.Println("\n")
-	fmt.Println(Verse(3))
-	fmt.Println("\n")
-	fmt.Println(Verse(4))
-	fmt.Println("\n")
-	fmt.Println(Verse(5))
-	fmt.Println("\n")
-	fmt.Println(Verse(6))
-	fmt.Println("\n")
-	fmt.Println(Verse(7))
-	fmt.Println("\n")
-	fmt.Println(Verse(8))
-	fmt.Println("\n")
+	fmt.Println(PowerBig(big.NewInt(2), big.NewInt(3)))
 }
--- diffie-hellman/diffie_hellman.go
+++ diffie-hellman/diffie_hellman.go
@@ -1,22 +1,76 @@
 package diffiehellman
 
-import "math/big"
+import (
+	"crypto/rand"
+	"math/big"
+)
 
+// ########################### NOTES ###############################
 // Diffie-Hellman-Merkle key exchange
 // Private keys should be generated randomly.
 
-func PrivateKey(p *big.Int) *big.Int {
-	panic("Please implement the PrivateKey function")
-}
+// Good tutorial:
+// Overview: https://www.youtube.com/watch?v=NmM9HA2MQGI
+// Math: https://www.youtube.com/watch?v=Yjrfm_oRO0w
+//	Big number: His video "n" = Our "p"
+//  Alice's a: 1 <= a <= n; It is random
+//	Bob's b: 1 <= b <= n; It is random
 
-func PublicKey(private, p *big.Int, g int64) *big.Int {
-	panic("Please implement the PublicKey function")
-}
+// Both calculate their Public Keys to share:
+// A = g^a mod n <-- So this ends up between 0 and n
+// B = g^b mod n
 
+// This is called the "Discrete log problem" - given A or B, finding the right a or b is extremely hard since it could be any number between 1 and n thanks to modulus - we can not know what it is.
+
+// Then, each of them take the Public Keys and raise it further
+// Alice: B^a mod n -> (g^a modn)^b mod n
+// Bob: A^b mod n -> (g^b modn)^a mod n
+// So - both are ending up with g^ab mod n
+
+// That's the shared secret key!
+
+// If some hacker tried to calculate using the public component g^a and g^b, they'd end up with:
+// g^a * g^b = g^(a+b)
+// Not the same as our shared secret key!
+// #################################################################
+
+// Alice and Bob use Diffie-Hellman key exchange to share secrets.
+// 1. They start with prime numbers
+// 2. Pick private keys
+// 3. Generate and share public keys
+// 4. Generate a shared secret key
+
+// 1. They start with prime numbers
+// We return (a,A) = (private, public)
 func NewPair(p *big.Int, g int64) (*big.Int, *big.Int) {
-	panic("Please implement the NewPair function")
+	a := PrivateKey(p)
+	A := PublicKey(a, p, g)
+	return a, A
 }
-
+// 2. Pick private keys
+func PrivateKey(p *big.Int) *big.Int {
+	return genBigRandNum(big.NewInt(2), p) // 2 <= a < p
+}
+// 3. Generate and share public keys
+func PublicKey(private, p *big.Int, g int64) *big.Int {
+	// https://pkg.go.dev/math/big#Int.Exp
+	return new(big.Int).Exp(big.NewInt(g), private, p) // (g^private) % p
+}
+// 4. Generate a shared secret key
 func SecretKey(private1, public2, p *big.Int) *big.Int {
-	panic("Please implement the SecretKey function")
+	return new(big.Int).Exp(public2, private1, p) // (public)^private % p
+}
+// Utility functions
+// Subtracts two "big" numbers
+func SubtractBigBfromA(B, A *big.Int) *big.Int {
+	return big.NewInt(0).Sub(B, A)
+}
+// Generates a random "big" number between min <= bg < max
+func genBigRandNum(min, max *big.Int) *big.Int {
+    bg := SubtractBigBfromA(max, min)
+    n, err := rand.Int(rand.Reader, bg)
+    if err != nil {
+        panic(err)
+    }
+    return big.NewInt(0).Add(n, min)
 }
