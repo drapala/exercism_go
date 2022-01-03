5b9adea1a27270454bab055d6423efd274fe4813
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2022-01-03 10:53:18 +0600

    Completed VLQ

--- _test/test.go
+++ _test/test.go
@@ -5,6 +5,7 @@ import (
 	"math"
 )
 
+// Starts from the left of a 32-bit integer and returns the first bit that has a 1.
 func getMax1Bit(val uint32) int {
 	for i:=31; i>=0; i-- { // Start from left
 		if getNthBit(val, uint32(i)) == 1 { // 0 index
@@ -14,6 +15,7 @@ func getMax1Bit(val uint32) int {
 	return -1
 }
 
+// Returns the value of the nth bit of a 32-bit integer.
 func getNthBit(val, n uint32) int {
     // 1. reverse the golang endian
     nthBit := n
@@ -26,6 +28,7 @@ func getNthBit(val, n uint32) int {
     // return (val >> (32-n)) & 1
 }
 
+// Convert a binary number to decimal for a given power of 2
 func calculateFromBinary(value, n int) int {
 	// Calculate 2 to the power of n
 	return value * int(math.Pow(float64(2), float64(n)))
@@ -40,39 +43,84 @@ func reverseSlice(input []byte) []byte {
 	return output
 }
 
-func main() {
-	input := []uint32{0x4000}
-	fmt.Println(input) // Print decimal value
-	fmt.Println(fmt.Sprintf("%08b", input)) // Print binary value
-	// 1 0000000 0000000
+// Convets a single unsigned 32 bit integer to VLQ representation
+func convertint32ToVLQ (input uint32) []byte {
+	// Handle zero case since we operate based on first non-zero nth byte here
+	if input == 0 {
+		return []byte{0x0}
+	} 
 
-	seven_bit := make([]byte, 0)
-	var c, nth, num int
+	vlq_bit := make([]byte, 0) // Return slice
+	var c, num_2, num_10 int // Intermediate variables
+	// c: used to track the 0-7 moving index
+	// num_2: value of the nth bit from binary conversion of input
+	// num_10: decimal value of num
 
-	for i:=0; i<=getMax1Bit(input[0]); i++ {
-		nth = getNthBit(input[0], uint32(i))
-		num += calculateFromBinary(nth, c)
-		c++
-		// 7th bit, reset
+	// Loop from right to left - until the last bit that is 1
+	for i:=0; i<=getMax1Bit(input); i++ {
+		num_2 = getNthBit(input, uint32(i)) // Get the nth bit from binary
+		num_10 += calculateFromBinary(num_2, c) // Convert and append to base 10
+		c++ // Increment c to keep track
+		// We've filled up the 7th bit, append and reset
 		if c == 7 { 
-			seven_bit = append(seven_bit, byte(num))
+			vlq_bit = append(vlq_bit, byte(num_10))
 			c = 0
-			num = 0
+			num_10 = 0
 		} 
-		// Last bit - append to slice
-		if i == getMax1Bit(input[0]){ 
-			seven_bit = append(seven_bit, byte(num))
+		// Last bit - may not be a factor of 7, so append to slice
+		// If c == 0, that means we just appended so no need to append again
+		if i == getMax1Bit(input) && c != 0 { 
+			vlq_bit = append(vlq_bit, byte(num_10))
 		}
 	}
-	seven_bit = reverseSlice(seven_bit)
+	vlq_bit = reverseSlice(vlq_bit) // Reverse slice to match output format
 
-	// Add MSB
-	for i:=0; i<len(seven_bit); i++ {
-		if i != len(seven_bit)-1 { // Not last bit - set MSB = 1
-			seven_bit[i] += byte(calculateFromBinary(1, 7))
+	// Add MSB bit
+	for i:=0; i<len(vlq_bit); i++ {
+		if i != len(vlq_bit)-1 { // Not last bit - set MSB = 1
+			vlq_bit[i] += byte(calculateFromBinary(1, 7))
 		}
-		fmt.Println(fmt.Sprintf("%x", seven_bit[i]))
 	}
-	// Print hex value
-	fmt.Println(fmt.Sprintf("%x", seven_bit)) // Print hex value
+	return vlq_bit
+}
+
+// Get the end of a VLQ sequence based on MSB = 0
+func getVLQend(byte_input []byte, track_left int) int {
+	for i:=track_left; i<len(byte_input); i++ {
+		if getNthBit(uint32(byte_input[i]), 7) == 0 { // MSB is 0 - end of VLQ
+			return i
+		}
+	}
+	return -1 // Not a valid VLQ
+}
+
+func main() {
+	// #######
+	// Decode
+	// #######
+	byte_input := []byte{0x8f, 0xff, 0xff, 0xff, 0x7f}
+	fmt.Printf("input: %x\n", byte_input)
+
+	var track_left, track_right int
+	var_int := make([]uint32, 0) // Return slice
+	var num_2, num_10, pow int
+
+	for true { // While loop, we break inside
+		track_right = getVLQend(byte_input, track_left)
+		if track_right == -1 { // Not a valid VLQ
+			break
+		}
+		// Loop right to left within this VLQ
+		for i:=track_right; i>=track_left; i-- {
+			for c:=0; c<=6; c++ { // Leave out the MSB
+				num_2 = getNthBit(uint32(byte_input[i]), uint32(c))
+				num_10 += calculateFromBinary(num_2, pow)
+				pow++ // Power of 2 increases right to left regardless of i or c
+			}
+		}
+		var_int = append(var_int, uint32(num_10)) // Append VLQ to slice
+		track_left = track_right + 1 // Move to the next VLQ
+		num_2, num_10, pow = 0, 0, 0 // Reset power of 2 for next VLQ run
+	}
+	fmt.Printf("output: %x", var_int)
 }
--- variable-length-quantity/variable_length_quantity.go
+++ variable-length-quantity/variable_length_quantity.go
@@ -1,19 +1,135 @@
 package variablelengthquantity
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func EncodeVarint(input []uint32) []byte {
-	result := make([]byte, 0)
-
-	fmt.Println(input)
-
-	return result
+// Starts from the left of a 32-bit integer and returns the first bit that has a 1.
+func getMax1Bit(val uint32) int {
+	for i:=31; i>=0; i-- { // Start from left
+		if getNthBit(val, uint32(i)) == 1 { // 0 index
+			return i // Return first bit that has a 1 - zero-index
+		}
+	}
+	return -1
 }
 
+// Returns the value of the nth bit of a 32-bit integer.
+func getNthBit(val, n uint32) int {
+    // 1. reverse the golang endian
+    nthBit := n
+    // 2. move the nth bit to the first position
+    movedVal := val >> nthBit
+    // 3. mask the value, selecting only this first bit
+    maskedValue := movedVal & 1
+    return int(maskedValue)
+    // can be shortened like so
+    // return (val >> (32-n)) & 1
+}
+
+// Convert a binary number to decimal for a given power of 2
+func calculateFromBinary(value, n int) int {
+	// Calculate 2 to the power of n
+	return value * int(math.Pow(float64(2), float64(n)))
+}
+
+// Reverse a slice
+func reverseSlice(input []byte) []byte {
+	output := make([]byte, 0)
+	for i:=len(input)-1; i>=0; i-- {
+		output = append(output, input[i])
+	}
+	return output
+}
+
+// Convets a single unsigned 32 bit integer to VLQ representation
+func convertint32ToVLQ (input uint32) []byte {
+	// Handle zero case since we operate based on first non-zero nth byte here
+	if input == 0 {
+		return []byte{0x0}
+	} 
+
+	vlq_bit := make([]byte, 0) // Return slice
+	var c, num_2, num_10 int // Intermediate variables
+	// c: used to track the 0-7 moving index
+	// num_2: value of the nth bit from binary conversion of input
+	// num_10: decimal value of num
+
+	// Loop from right to left - until the last bit that is 1
+	for i:=0; i<=getMax1Bit(input); i++ {
+		num_2 = getNthBit(input, uint32(i)) // Get the nth bit from binary
+		num_10 += calculateFromBinary(num_2, c) // Convert and append to base 10
+		c++ // Increment c to keep track
+		// We've filled up the 7th bit, append and reset
+		if c == 7 { 
+			vlq_bit = append(vlq_bit, byte(num_10))
+			c = 0
+			num_10 = 0
+		} 
+		// Last bit - may not be a factor of 7, so append to slice
+		// If c == 0, that means we just appended so no need to append again
+		if i == getMax1Bit(input) && c != 0 { 
+			vlq_bit = append(vlq_bit, byte(num_10))
+		}
+	}
+	vlq_bit = reverseSlice(vlq_bit) // Reverse slice to match output format
+
+	// Add MSB bit
+	for i:=0; i<len(vlq_bit); i++ {
+		if i != len(vlq_bit)-1 { // Not last bit - set MSB = 1
+			vlq_bit[i] += byte(calculateFromBinary(1, 7))
+		}
+	}
+	return vlq_bit
+}
+
+// Get the end of a VLQ sequence based on MSB = 0
+func getVLQend(byte_input []byte, track_left int) int {
+	for i:=track_left; i<len(byte_input); i++ {
+		if getNthBit(uint32(byte_input[i]), 7) == 0 { // MSB is 0 - end of VLQ
+			return i
+		}
+	}
+	return -1 // Not a valid VLQ
+}
+
+// Encodes an array of unsigned 32 bit integers to VLQ representation
+func EncodeVarint(input []uint32) []byte {
+	vlq_output := make([]byte, 0) // Return slice
+
+	for _, num := range(input) {
+		vlq_output = append(vlq_output, convertint32ToVLQ(num)...)
+	}
+	return vlq_output
+}
+
+// Decodes an array of VLQ representations to unsigned 32 bit integers
 func DecodeVarint(input []byte) ([]uint32, error) {
-	result := make([]uint32, 0)
+	var track_left, track_right int
+	var_int := make([]uint32, 0) // Return slice
+	var num_2, num_10, pow int
 
-	fmt.Println(input)
+	for true { // While loop, we break inside
+		track_right = getVLQend(input, track_left)
+		if track_right == -1 { // Not a valid VLQ
+			break
+		}
+		// Loop right to left within this VLQ
+		for i:=track_right; i>=track_left; i-- {
+			for c:=0; c<=6; c++ { // Leave out the MSB
+				num_2 = getNthBit(uint32(input[i]), uint32(c))
+				num_10 += calculateFromBinary(num_2, pow)
+				pow++ // Power of 2 increases right to left regardless of i or c
+			}
+		}
+		var_int = append(var_int, uint32(num_10)) // Append VLQ to slice
+		track_left = track_right + 1 // Move to the next VLQ
+		num_2, num_10, pow = 0, 0, 0 // Reset power of 2 for next VLQ run
+	}
 
-	return result, nil
+	if len(var_int) == 0 && len(input)!= 0{ // No valid VLQs
+		return nil, fmt.Errorf("no valid VLQs")
+	}
+	return var_int, nil
 }
