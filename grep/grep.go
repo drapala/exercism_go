df7d16ac6a6dda3b3d5baf76edb6727d9296ed4e
Raki Rahman <mdrrahman@microsoft.com>
2022-01-21 19:45:52 -0500

    Grep level 1

--- grep/grep.go
+++ grep/grep.go
@@ -1,5 +1,54 @@
 package grep
 
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+// flags:
+// -n Print the line numbers of each matching line.
+// -l Print only the names of files that contain at least one matching line.
+// -i Match line using a case-insensitive comparison.
+// -v Invert the program -- collect all lines that fail to match the pattern.
+// -x Only match entire lines, instead of lines that contain a match.
 func Search(pattern string, flags, files []string) []string {
-	panic("Please implement the Search function")
+	fmt.Println("Searching for pattern:", pattern)
+	fmt.Println("Flags:", flags)
+	fmt.Println("Files:", files)
+
+	fileMap := Files(fileContentData)
+
+	// The files we're interested in
+	for _, file := range files {
+		fmt.Println(file, fileMap[file])
+	}
+
+	return []string{}
+}
+
+// Returns a key of slices containing the file contents, where:
+// 1. The leading and trailing whitespaces removed
+// 2. Beautifying characters removed
+func Files(fileContentData []string) map[string][]string {
+	result := make(map[string][]string)
+
+	var key string
+	for _, line := range fileContentData {
+		// Trim leading and trailing whitespaces
+		line = strings.TrimSpace(line)
+		if regexp.MustCompile(`.txt`).FindAllStringIndex(line, -1) != nil { // Key
+			key = line // Set key for subsequent lines
+		} else if regexp.MustCompile(`------`).FindAllStringIndex(line, -1) != nil { //divider
+			// Do nothing
+		} else { // Content
+			// Replace "|" with " "
+			line = strings.Replace(line, "|", "", -1)
+			// Trim leading and trailing whitespaces after | cleanup
+			line = strings.TrimSpace(line)
+			// Get content in result for key
+			result[key] = append(result[key], line)
+		}
+	}
+	return result
 }
