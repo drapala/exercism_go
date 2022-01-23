a5ea641515074e8f7b0e6e83d276767f389c82d6
Raki Rahman <mdrrahman@microsoft.com>
2022-01-23 08:18:47 -0500

    Completed House Song

--- house/house.go
+++ house/house.go
@@ -1,7 +1,9 @@
 package house
 
+import "fmt"
+
 var nounToActionMap = map[string]string{
-	"house that Jack built":            "",
+	"house that Jack built.":           "",
 	"malt":                             "lay in the",
 	"rat":                              "ate the",
 	"cat":                              "killed the",
@@ -14,12 +16,28 @@ var nounToActionMap = map[string]string{
 	"farmer sowing his corn":           "kept the",
 	"horse and the hound and the horn": "belonged to the",
 }
-var sequence = []string{"house that Jack built", "malt", "rat", "cat", "dog", "cow with the crumpled horn", "maiden all forlorn", "man all tattered and torn", "priest all shaven and shorn", "rooster that crowed in the morn", "farmer sowing his corn", "horse and the hound and the horn"}
+var sequence = []string{"house that Jack built.", "malt", "rat", "cat", "dog", "cow with the crumpled horn", "maiden all forlorn", "man all tattered and torn", "priest all shaven and shorn", "rooster that crowed in the morn", "farmer sowing his corn", "horse and the hound and the horn"}
 
 func Verse(v int) string {
-	panic("Please implement the Verse function")
+	stack := sequence[:v]
+	var result string
+	for i := v - 1; i >= 0; i-- {
+		if i == v-1 {
+			result += fmt.Sprintf("This is the %s", stack[i])
+		} else {
+			result += fmt.Sprintf("\nthat %s %s", nounToActionMap[stack[i+1]], stack[i])
+		}
+	}
+	return result
 }
 
 func Song() string {
-	panic("Please implement the Song function")
+	var result string
+	for i := 1; i <= len(sequence); i++ {
+		result += Verse(i)
+		if i != len(sequence) {
+			result += "\n\n"
+		}
+	}
+	return result
 }
