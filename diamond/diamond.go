5906983c20f0d672967c412b98b0d6ef622758bd
Raki Rahman <mdrrahman@microsoft.com>
2022-01-28 14:52:56 -0500

    Solved diamond!

--- diamond/diamond.go
+++ diamond/diamond.go
@@ -2,7 +2,6 @@ package diamond
 
 import (
 	"fmt"
-	"time"
 )
 
 type board struct {
@@ -11,25 +10,23 @@ type board struct {
 	char      byte   // Char to get to before flipping
 }
 
-func needToInsert(pos, i, row int, flip bool) bool {
-	fmt.Println("flip?", flip)
+func needToInsert(pos, i, row, dimension int, flip bool) bool {
 	if !flip { // Up until halfway point
 		if pos == (i-(row-1)) || pos == (i+(row-1)) {
 			return true
 		}
 		return false
 	} else { // Past the halfway point
-		// Continue from here
+		if pos == (i+row-dimension) || pos == (i-row+dimension) {
+			return true
+		}
 	}
 	return false // Default
 }
 
-func (b board) generateBoard() {
+func (b *board) generateBoard() {
 	b.rows = ""                 // To contain finished board
 	i := int(b.char) - int('A') // Distance from A
-	fmt.Println("Distance from A: ", i)
-	fmt.Println("Board dimension: ", b.dimension)
-	fmt.Println("Halfway at:", string(rune(int(b.char))))
 
 	var ascii int         // Ascii of current row
 	var flip bool = false // Reached halfway point
@@ -37,34 +34,35 @@ func (b board) generateBoard() {
 
 	for row := 1; row <= b.dimension; row++ { // Loop through rows
 		for pos := 0; pos < b.dimension; pos++ { // Loop through positions
-			if needToInsert(pos, i, row, flip) { // If we need to insert character
+			if needToInsert(pos, i, row, b.dimension, flip) { // If we need to insert character
 				if flip { // If we are past halfway point
 					ascii = int(b.char) - (row - half)
+					b.rows += string(rune(ascii))
 				} else {
 					ascii = int('A') + row - 1
 					b.rows += string(rune(ascii))
 				}
-				if ascii == int(b.char) && pos == (i+(row-1)) { // Halfway point after second write
+				if ascii == int(b.char) && pos == (i+(row-1)) { // Mark Halfway point after second write
 					flip = true
 					half = row
 				}
 			} else {
-				b.rows += "." // Change to space after
+				b.rows += " " // Change to space after
 			}
 		}
 		// After looping through the positions
 		b.rows += "\n"
-		fmt.Println(b.rows)
-		time.Sleep(500 * time.Millisecond)
 	}
-	// Go to sleep
-	// time.Sleep(10 * time.Second)
 }
 
 func Gen(char byte) (string, error) {
+	// Distance from A
+	input_num := int(char) - int('A')
 
-	fmt.Println("input:", string(char))
-	input_num := int(char) - int('A') // Distance from A
+	// Error handling
+	if input_num < 0 || input_num > 25 { // Outside of A - Z
+		return "", fmt.Errorf("invalid input: %s", string(char))
+	}
 
 	// Generate empty board
 	my_board := board{}
@@ -74,7 +72,5 @@ func Gen(char byte) (string, error) {
 	// Fill board
 	my_board.generateBoard()
 
-	// Error handle outsize Z
-
-	return "", nil
+	return my_board.rows, nil
 }
