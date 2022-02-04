59c1d3dc122eee149e9399794412833b0a01545a
Raki Rahman <mdrrahman@microsoft.com>
2022-02-04 10:55:09 -0500

    Completed Queen Attack

--- queen-attack/queen_attack.go
+++ queen-attack/queen_attack.go
@@ -1,5 +1,74 @@
 package queenattack
 
+import (
+	"errors"
+	"math"
+)
+
+// Create map of letters to column number
+var col_map = map[string]int{
+	"a": 0,
+	"b": 1,
+	"c": 2,
+	"d": 3,
+	"e": 4,
+	"f": 5,
+	"g": 6,
+	"h": 7,
+}
+
+var row_map = map[string]int{
+	"8": 0,
+	"7": 1,
+	"6": 2,
+	"5": 3,
+	"4": 4,
+	"3": 5,
+	"2": 6,
+	"1": 7,
+}
+
+func pos2array(pos string) (int, int, error) {
+	// Error handling
+	if len(pos) != 2 { // Not 2 characters
+		return 0, 0, errors.New("invalid length of position")
+	}
+	// 2 characters from this point on
+	firstChar := pos[0]
+	secondChar := pos[1]
+	// Error handling
+	if firstChar < 'a' || firstChar > 'h' { // First char not in range
+		return 0, 0, errors.New("invalid first character of position")
+	} else if secondChar < '1' || secondChar > '8' { // Second char not in range
+		return 0, 0, errors.New("invalid second character of position")
+	}
+	// Get index mapping
+	row, col := col_map[string(firstChar)], row_map[string(secondChar)]
+	return row, col, nil
+}
+
+func CheckIfDiagonal(w_r, w_c, b_r, b_c int) bool {
+	// Check if on same diagonal
+	return math.Abs(float64(w_r-b_r)) == math.Abs(float64(w_c-b_c))
+}
+
 func CanQueenAttack(whitePosition, blackPosition string) (bool, error) {
-	panic("Please implement the CanQueenAttack function")
+	w_r, w_c, w_err := pos2array(whitePosition)
+	b_r, b_c, b_err := pos2array(blackPosition)
+	// Error handling
+	if w_err != nil {
+		return false, w_err
+	} else if b_err != nil {
+		return false, b_err
+	} else if whitePosition == blackPosition { // Same position, no attack
+		return false, errors.New("same position")
+	}
+	// Same row or column
+	if w_c == b_c { // Check if on same column
+		return true, nil
+	} else if w_r == b_r { // Check if on same row
+		return true, nil
+	}
+	// Check if on same diagonal
+	return CheckIfDiagonal(w_r, w_c, b_r, b_c), nil
 }
--- queen-attack/queen_attack_test.go
+++ queen-attack/queen_attack_test.go
@@ -10,23 +10,32 @@ var tests = []struct {
 	attack bool
 	ok     bool
 }{
-	{"b4", "b4", false, false}, // same square
+	// Error cases
 	{"a8", "b9", false, false}, // off board
+	{"", "", false, false},
+	{"here", "there", false, false}, // invalid
+	{"b4", "b4", false, false},      // same square
 	{"a0", "b1", false, false},
 	{"g3", "i5", false, false},
-	{"here", "there", false, false}, // invalid
-	{"", "", false, false},
 
-	{"b3", "d7", false, true}, // no attack
-	{"a1", "f8", false, true},
+	// Proper cases:
+	// Same column
 	{"b4", "b7", true, true}, // same file
+
+	// Same row:
 	{"e4", "b4", true, true}, // same rank
+
+	// Diagonal?
 	{"a1", "f6", true, true}, // common diagonals
 	{"a6", "b7", true, true},
 	{"d1", "f3", true, true},
 	{"f1", "a6", true, true},
 	{"a1", "h8", true, true},
 	{"a8", "h1", true, true},
+
+	// No hit:
+	{"b3", "d7", false, true}, // no attack
+	{"a1", "f8", false, true},
 }
 
 func TestCanQueenAttack(t *testing.T) {
